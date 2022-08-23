38e35f8b660bf72ee2521b30c78a5fcf9b16e00a
Harsh Mishra <hmisraji07@gmail.com>
2022-08-23 19:22:57 +0530

    Views and Serializers for API Added

---
+++ quiz/serializers.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+from .models import Quiz, Question, Answer
+
+class QuizSerializer(serializers.ModelSerializer):
+    #Serialize quiz Data
+     class Meta:
+         model = Quiz
+         fields = [
+             'title',
+         ]
+class AnswerSerializer(serializers.ModelSerializer):
+    #Serialize Answers Data
+    class Meta:
+        model = Answer
+        fields =[
+            'id',
+            'ans_text',
+            'is_right',
+        ]
+
+class QuestionSerializer(serializers.ModelSerializer):
+    #Serialize Question w/ Quiz Data
+    answers = AnswerSerializer(many=True, read_only=True)
+    quiz = QuizSerializer(read_only=True)
+    class Meta:
+        model = Question
+        fields = [
+            'quiz',
+            'title', 
+            'answer',
+        ]
+
+class RandomQuestionSerializer(serializers.ModelSerializer):
+    #to serialize fetched questions reagardless of quiz
+    answers =  AnswerSerializer(many=True, read_only=True)
+    class Meta:
+        model = Question
+        fields =[
+            'title',
+            'answer',
+        ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
-
+from rest_framework import generics
+from .models import Quiz,Question
+from .serializers import QuestionSerializer, QuizSerializer, RandomQuestionSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
+
+class QuizView(generics.ListAPIView):
+    serializer_class = QuizSerializer
+    queryset = Quiz.objects.all()
+
+class GetRandomQuestion(APIView):
+    
+    def get(self, request, format=None, **kwargs):
+        question = Question.objects.filter(quiz__title=kwargs['topic']).order_by('?')[:1] 
+        serializer = RandomQuestionSerializer(question, many=True)
+        return Response(serializer.data)
+
+class GetQuizQuestion(APIView):
+
+    def get(self, request, format=None, **kwargs):
+        quiz_questions=Question.objects.filter(quiz__title=kwargs['topic'])
+        serializer = QuestionSerializer(quiz_questions, amny=True)
+        return Response(serializer.data)
